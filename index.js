c0a9c843520414b392ae2c34a60f379fbe46d770
delta12kilo <dk9044552577@gmail.com>
2020-08-12 13:59:08 +0530

    change greeting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send({hi: 'there'});
+    res.send({by: 'there'});
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
