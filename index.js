d681b96523d7f1fd87831538883acf29a7b7c341
delta12kilo <dk9044552577@gmail.com>
2020-08-12 16:47:42 +0530

    adding passport and google OAuth

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 // import express from 'express';
-
+const passport  = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
 // name of app is "app"
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send({by: 'there'});
-});
+passport.use(new googleStrategy());
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
