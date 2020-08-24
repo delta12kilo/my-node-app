9272a0b70fdb8ff971fb289ab72a149e747d83d3
delta12kilo <dk9044552577@gmail.com>
2020-08-24 12:08:10 +0530

    new changes

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-// import express from 'express';
-
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
-
 
 // name of app is "app"
 const app = express();
-
-
 require('./routes/authRoutes')(app);
 
-
+//mongo connection
+mongoose.connect(keys.mongoURI);
+// console.log('connect');
+//////////////////////////////
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
--- services/passport.js
+++ services/passport.js
@@ -2,18 +2,26 @@
 const passport  = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
 
-
+const User = mongoose.model('users')
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 },(accesToken, refresh,profile,done)=>{
-        console.log('acces token:', accesToken);
-        console.log('profile:',profile);
-        console.log('refresh token:', refresh);
-    }
+        User.findOne({ googleId: profile.id }).then(existingUser => {
+                if(existingUser){
+                    done(null,existingUser);
+                    //we already have user
+                }
+                else{
+                    //we dont have user with this id
+                    new User({ googleId: profile.id }).save()
+                        .then(user => done(null,user));
+                }
+            });   
+        }
+    )
 )
-);
-
